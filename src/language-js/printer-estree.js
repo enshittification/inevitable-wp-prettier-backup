f43d057ab06a6e5c3fad62c6f393aee168090f78
Jarda Snajdr <jsnajdr@gmail.com>
2017-07-14 09:37:39 +0200

    Fix parens around LogicalExpression wrapped inside UnaryExpression or MemberExpression
    Before:
    ```js
    ! (
      a || b
     )
    ```
    After:
    ```js
    ! (
      a || b
    )
    ```
    Fixed by inserting the parens directly when formatting a Binary or LogicalExpression instead
    of relying on `genericPrint` inserting them depending on result of `path.needParens`.
    
    Also fixes parens about MemberExpression object:
    ```js
    (
      a || b
    ).c;
    ```
    and semicolon insertion in no-semi mode for MemberExpressions:
    ```js
    x
    ;(a||b).c
    ```
    
    All unit tests continue to pass (including the Flow and Typescript ones).

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -481,7 +481,12 @@ function printPathNoParens(path, options, print, args) {
           !parent.computed)
       ) {
         return group(
-          concat([indent(concat([softline, concat(parts)])), softline])
+          concat([
+            "(",
+            indent(concat([parenLine, concat(parts)])),
+            parenLine,
+            ")"
+          ])
         );
       }
 
@@ -5485,7 +5490,12 @@ function exprNeedsASIProtection(path, options) {
     node.type === "BindExpression" ||
     node.type === "RegExpLiteral" ||
     (node.type === "Literal" && node.pattern) ||
-    (node.type === "Literal" && node.regex);
+    (node.type === "Literal" && node.regex) ||
+    // needsParens is false for binaryish expr inside memberexpr, but is parenthesized nevertheless
+    (node.type === "MemberExpression" &&
+      !node.computed &&
+      (node.object.type === "LogicalExpression" ||
+        node.object.type === "BinaryExpression"));
 
   if (maybeASIProblem) {
     return true;
