0c0ee5e9d5aa4f3ced57ef4ee7961cccb2bac6c5
Jarda Snajdr <jsnajdr@gmail.com>
2018-04-27 21:53:30 +0200

    Print spaces in parenthesized CSS pseudo-selectors

--- src/language-css/printer-postcss.js
+++ src/language-css/printer-postcss.js
@@ -379,10 +379,17 @@ function genericPrint(path, options, print) {
       return group(indent(concat(path.map(print, "nodes"))));
     }
     case "selector-pseudo": {
+      const parenSpace = options.parenSpacing ? " " : "";
       return concat([
         maybeToLowerCase(node.value),
         node.nodes && node.nodes.length > 0
-          ? concat(["(", join(", ", path.map(print, "nodes")), ")"])
+          ? concat([
+              "(",
+              parenSpace,
+              join(", ", path.map(print, "nodes")),
+              parenSpace,
+              ")"
+            ])
           : ""
       ]);
     }
